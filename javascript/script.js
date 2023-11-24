a9e0721c9d6bb249baacf65afdcd88894a335e01
Braian Mena <braianmena@gmail.com>
2023-11-24 15:47:20 -0300

    Agregado de enunciado del trabajo.

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+// Clase de Práctica
+
+// 1.- Crear un repo en github.
+// 2.- Crear la rama dev.
+// 3.- Vamos a tener solo un archivo index.html y un archivo script.js
+// 4.- Crear una rama por cada integrante del equipo.
+// 5.- Mergeamos la branch a dev.
+
+// Trabajo en equipo
+// -> Carrito de productos
+// Carrito de compras con Array:
+//      *Crear una aplicadion que de carrito de compras donde se ingresen productos como elementos en un array.
+//      *Las tareas que debe hacer son las siguientes:
+//          1.) Agregar productos al carrito.
+//          2.) Listar los productos del carrito.
+//          3.) Buscar un producto en el carrito.
+//          4.) Filtrar productos que coincidan con una palabra, parte de ella o una letra.
+//          5.) Eliminar producto del carrito.
+
+
