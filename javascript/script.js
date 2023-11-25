2b850735e5f97331b4c18f0ac7058023cbf54a00
Braian Mena <braianmena@gmail.com>
2023-11-24 22:04:03 -0300

    Desarrollo y finalizacion del práctico.

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@
 // Trabajo en equipo
 // -> Carrito de productos
 // Carrito de compras con Array:
-//      *Crear una aplicadion que de carrito de compras donde se ingresen productos como elementos en un array.
+//      *Crear una aplicación de carrito de compras donde se ingresen productos como elementos en un array.
 //      *Las tareas que debe hacer son las siguientes:
 //          1.) Agregar productos al carrito.
 //          2.) Listar los productos del carrito.
@@ -18,3 +18,77 @@
 //          5.) Eliminar producto del carrito.
 
 
+
+// 1-> Agregar productos al carrito.
+let agregarProductos = carrito => {
+    let producto;
+
+    do{
+        producto = prompt('Ingrese el producto que desea agregar al carrito:\n*Si ya finalizó, presione la tecla \'Enter\'');
+        
+        if(producto !== '' && producto !== null){
+            carrito.push(producto);
+        }
+
+    }while(producto !== '' && producto !== null)
+
+}
+
+
+// 2-> Listar los productos del carrito.
+let listarProductos = carrito => {
+    console.log("\n\t>>>>>>============////// PRODUCTOS DEL CARRITO //////============<<<<<<");
+    for(let i=0; i<carrito.length; i++){
+        console.log(`\t${i+1}.- ${carrito[i]}`);
+    }
+}
+
+
+// 3-> Buscar un producto en el carrito. ==> Supongo que debe devolver la primera ocurrencia.
+let buscarProducto = carrito => {
+    let producto = prompt("Ingrese el dato que desea buscar:").replace(/\s/g,'').split('').join('').toLowerCase();
+    for(let i=0; i<carrito.length; i++){
+        if(carrito[i].toLowerCase() === producto){
+            console.log(`\tSe encontró el producto ${i+1}.- ${carrito[i]}`);
+            return
+        }
+    }
+    alert("\tNO SE ENCONTRO LA OCURRENCIA");
+}
+
+
+// 4-> Filtrar productos que coincidan con una palabra, parte de ella o una letra.
+let filtrarProducto = carrito => {
+    let producto = prompt("Ingrese el dato que desea filtrar:").replace(/\s/g,'').split('').join('').toLowerCase();
+    let aux=0;
+    for(let i=0; i<carrito.length; i++){
+        if(carrito[i].toLowerCase().includes(producto)){
+            console.log(`\tProducto ${i+1}.- ${carrito[i]}`);
+            aux++;
+        }
+    }
+    if(aux===0) alert("\tNO SE ENCONTRO LA OCURRENCIA");
+}
+
+
+// 5-> Eliminar producto del carrito. ==> Se considera que debe ser la ocurrencia exacta, y se elimina solo 1 producto.
+let eliminarProducto = carrito => {
+    let producto = prompt("Ingrese el dato que desea eliminar:").replace(/\s/g,'').split('').join('').toLowerCase();
+    for(let i=0; i<carrito.length; i++){
+        if(carrito[i].toLowerCase() === producto){
+            return carrito.splice(i,1);
+        }
+    }
+    alert("\tNO SE ENCONTRO LA OCURRENCIA");
+}
+
+
+let productos = ["manzana", "pera", "magoya"];
+
+agregarProductos(productos);
+listarProductos(productos);
+buscarProducto(productos);
+filtrarProducto(productos);
+eliminarProducto(productos);
+
+console.log(productos);
